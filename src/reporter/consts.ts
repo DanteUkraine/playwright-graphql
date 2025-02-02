2dc6dea788f8544df71a3abbf269dd7589545633
Oleksandr Solomin <113831103+DanteUkraine@users.noreply.github.com>
2025-02-02 17:33:32 +0200

    Update src/reporter/consts.ts
    Co-authored-by: Andrii Dembitskyi <andrew.dembitskiy@gmail.com>

--- src/reporter/consts.ts
+++ src/reporter/consts.ts
@@ -1,3 +1,3 @@
 import { join } from 'path';
 
-export const coverageDir = join(process.cwd(), process.env.PW_GQL_TEMP_DIR?? '.gql-coverage');
+export const coverageDir = join(process.cwd(), process.env.PW_GQL_TEMP_DIR ?? '.gql-coverage');
