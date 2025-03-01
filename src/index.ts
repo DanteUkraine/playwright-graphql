893460429190cf533f3e6e03256b979a981863ef
DanteUkraine <osolomin60@outlook.com>
2025-03-02 00:04:42 +0200

    fix for import

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { getSdkRequester } from './requester';
-export { coverageLogger } from './coverage-logger';
+import { getSdkRequester } from './requester';
+import { coverageLogger } from './coverage-logger';
+
+export { getSdkRequester, coverageLogger };
