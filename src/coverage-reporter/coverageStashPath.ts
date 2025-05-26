71990e9f53179415b6cc8953d8a11a2d0ab5762d
DanteUkraine <osolomin60@outlook.com>
2025-05-26 11:30:36 +0300

    fix/windows-compatibility

--- src/coverage-reporter/coverageStashPath.ts
+++ src/coverage-reporter/coverageStashPath.ts
@@ -1,8 +1,8 @@
-import { join } from 'path';
+import { resolve } from 'path';
 
 export function getPathToTmpCoverageStash(path: string): string {
     const parts = path.split('/');
     const fileWithExtension = parts[parts.length - 1];
 
-    return join(process.cwd(), `.${fileWithExtension.replace('.ts', '')}-coverage`);
+    return resolve(process.cwd(), `.${fileWithExtension.replace('.ts', '')}-coverage`);
 }
